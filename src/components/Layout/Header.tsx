7fedb505531e89f3c3ea9baa13c303dd75deade7
momotelouet <momotelouet@gmail.com>
2025-06-27 08:41:54 +0100

    Update Header.tsx

--- src/components/Layout/Header.tsx
+++ src/components/Layout/Header.tsx
@@ -15,7 +15,7 @@ const Header = () => {
           <div className="container mx-auto px-4 py-4">
             <div className="flex items-center justify-between">
               <img 
-                src="/lovable-uploads/79fb5607-6b8b-41b6-97e5-20f16492e405.png" 
+                src="/eemploi-logo.png" 
                 alt="eemploi logo" 
                 className="h-8 w-auto"
               />
